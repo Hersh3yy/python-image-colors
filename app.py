7cb99255de093ab35e23ce86e60aafac10fe9733
HirenBudhAir <hirenbudhrani@gmail.com>
2023-07-20 01:55:39 +0200

    more colors

--- app.py
+++ app.py
@@ -89,7 +89,7 @@ def analyze():
         img_np = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
 
         # Get the color palette
-        palette = get_color_palette(img_np, 10)
+        palette = get_color_palette(img_np, 20)
 
         # Return the palette as a JSON response
         logging.info(f'Entire request took: {time.time() - start_time} seconds')
