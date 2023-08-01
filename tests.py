c8ff979a887e872906075cfa3485abb0ebc84a02
HirenBudhAir <hirenbudhrani@gmail.com>
2023-08-02 00:05:26 +0200

    functional tests

--- tests.py
+++ tests.py
@@ -1,12 +1,20 @@
 import json
 import unittest
 from flask_testing import TestCase
-from your_flask_app_file import app  # Import the Flask app
+from app import app  # Import the Flask app
+import logging
 
 class FlaskAppTest(TestCase):
     def create_app(self):
         app.config['TESTING'] = True
         return app
+    
+    def setUp(self):
+        logging.basicConfig(level=logging.INFO)
+        logging.info("Setting up the test case")
+
+    def tearDown(self):
+        logging.info("Tearing down the test case")
 
     def test_hello_world(self):
         response = self.client.get('/test')
@@ -28,6 +36,17 @@ class FlaskAppTest(TestCase):
         self.assertEqual(response.status_code, 400)
         data = json.loads(response.data)
         self.assertEqual(data['error'], 'Please provide r, g and b values')
+    
+    def test_get_closest_color(self):
+        response = self.client.get('/closest_color', query_string={'r': 0, 'g': 0, 'b': 0})
+        data = json.loads(response.data.decode())
+
+        # Assert the response status code
+        self.assertEqual(response.status_code, 200)
+
+        # Assert that the returned color is what you expect (this will depend on your color database)
+        # Here we assume black has the color name 'black'
+        self.assertEqual(data['color_name'], 'black')
 
 if __name__ == '__main__':
     unittest.main()
