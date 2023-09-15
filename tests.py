8538339bb0007ed08c7304679cd8e485a6f2e886
HirenBudhAir <hirenbudhrani@gmail.com>
2023-09-15 02:21:29 +0200

    update tests to code changes

--- tests.py
+++ tests.py
@@ -26,27 +26,27 @@ class FlaskAppTest(TestCase):
         self.assertEqual(response.data, b'No file part')
 
     def test_closest_color_invalid_hex(self):
-        response = self.client.get('/closest_color', query_string={'hex': 'invalid'})
+        response = self.client.get('/closest_color_rgb', query_string={'hex': 'invalid'})
         self.assertEqual(response.status_code, 400)
         data = json.loads(response.data)
         self.assertEqual(data['error'], 'Invalid hex color code')
 
     def test_closest_color_no_color(self):
-        response = self.client.get('/closest_color')
+        response = self.client.get('/closest_color_rgb')
         self.assertEqual(response.status_code, 400)
         data = json.loads(response.data)
         self.assertEqual(data['error'], 'Please provide r, g and b values')
     
     def test_get_closest_color(self):
-        response = self.client.get('/closest_color', query_string={'r': 0, 'g': 0, 'b': 0})
+        response = self.client.get('/closest_color_lab', query_string={'r': 0, 'g': 0, 'b': 0})
         data = json.loads(response.data.decode())
 
         # Assert the response status code
         self.assertEqual(response.status_code, 200)
 
         # Assert that the returned color is what you expect (this will depend on your color database)
-        # Here we assume black has the color name 'black'
-        self.assertEqual(data['color_name'], 'black')
+        # Here we assume black has the color name 'Black'
+        self.assertEqual(data['color_name'], 'Black')
 
 if __name__ == '__main__':
     unittest.main()
