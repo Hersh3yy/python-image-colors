ddfbe2ef2fff79fe10ac95ba9b1a1dd03c60d414
HirenBudhAir <hirenbudhrani@gmail.com>
2023-11-14 14:30:57 +0100

    backstage pantone updates

--- cisg.py
+++ cisg.py
@@ -1,11 +1,9 @@
 # Color Insert Script Generator || CISG
 import logging
 import psycopg2
-import os
 import argparse
-from color_names import parent_colors
 from pantone_numbers import color_names
-from color_utis import insert_parent_color_sql_command, insert_color_name_sql_command
+from color_utis import insert_color_name_sql_command
 
 logging.basicConfig(level=logging.INFO)
 
@@ -15,43 +13,9 @@ parser = argparse.ArgumentParser(description="Insert color data based on a speci
 parser.add_argument('color_space', choices=['rgb', 'hsl', 'cmyk', 'pantone', 'lab'], help='Specify the color space.')
 args = parser.parse_args()
 
-parent_colors_dict = {hex: name for hex, name in parent_colors}
-
-# ID Generation Loop for parent colors
-logging.info('Generating IDs for parent colors')
-parent_ids = {}
-parent_id_counter = 1
-for hex, name in parent_colors:
-    parent_ids[name] = parent_id_counter
-    parent_id_counter += 1
-
-logging.warning("parent colors ids array")
-logging.warning(parent_ids)
-
-# Insertion Loop for parent colors
-logging.info('Starting to insert parent colors')
-for hex, name in parent_colors:
-    # Use the new function for parent colors
-    insert_parent_color_sql_command((hex, name), args.color_space, parent_ids[name])
-
 # Insertion Loop for color names
 logging.info('Starting to insert color names')
 for pantone, color_info in color_names.items():
-    insert_color_name_sql_command((color_info['hex'], color_info['name']), args.color_space, parent_ids, parent_colors_dict, pantone)
+    insert_color_name_sql_command((color_info['hex'], color_info['name']), args.color_space, pantone)
 
 logging.info('Finished inserting color names')
-
-# Function to establish a connection with the PostgreSQL database
-def connect_to_db():
-    try:
-        connection = psycopg2.connect(
-            dbname=os.getenv("DB_NAME"),
-            user=os.getenv("DB_USER"),
-            password=os.getenv("DB_PASSWORD"),
-            host=os.getenv("DB_HOST"),
-            port=os.getenv("DB_PORT")
-        )
-        return connection
-    except Exception as error:
-        logging.error(f"Error while connecting to PostgreSQL: {error}")
-        exit(1)
--- color_utis.py
+++ color_utis.py
@@ -2,6 +2,8 @@ import webcolors
 from colormath.color_objects import sRGBColor, LabColor, CMYKColor, XYZColor
 from colormath.color_conversions import convert_color
 import logging
+import os
+from color_names import parent_colors
 
 # Function to calculate Euclidean distance in any color space
 def calculate_euclidean_distance(color1, color2):
@@ -37,56 +39,56 @@ color_space_functions = {
     'cmyk': lambda hex: hex_to_cmyk(hex),
 }
 
+# Function to establish a connection with the PostgreSQL database
+def connect_to_db():
+    try:
+        connection = psycopg2.connect(
+            dbname=os.getenv("DB_NAME"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            host=os.getenv("DB_HOST"),
+            port=os.getenv("DB_PORT")
+        )
+        return connection
+    except Exception as error:
+        logging.error(f"Error while connecting to PostgreSQL: {error}")
+        exit(1)
+
 # Function to find the closest color given a requested color and color space
 def closest_color_in_space(requested_color, parent_colors, color_space):
 
     requestedHex, name = requested_color
     min_distances = {}
-    color_space_fn = color_space_functions.get(color_space, hex_to_rgb)
+    color_space_fn = color_space_functions.get(color_space, hex_to_lab)
 
     for hex, name in parent_colors.items():
         distance = calculate_euclidean_distance(
             color_space_fn(requestedHex),
             color_space_fn(hex)
         )
-        min_distances[distance] = name
+        min_distances[distance] = hex, name
 
     # Sort and get closest color
-    closest_color = min(min_distances.items())[1]
+    logging.info(min_distances.items())
+    logging.info(min(min_distances.items()))
+    closest_color = min(min_distances.items())
 
     logging.info(f"Closest found {color_space} color for {name}: {closest_color}")
     return closest_color
 
-def insert_parent_color_sql_command(color_info, color_space, parent_id):  # Added parent_id argument
+parent_colors_dict = {hex: name for hex, name in parent_colors}
+
+def insert_color_name_sql_command(color_info, color_space, pantone):
     hex, name = color_info
     r, g, b = hex_to_rgb(hex)
     lab_l, lab_a, lab_b = hex_to_lab(hex)
     c, m, y, k = hex_to_cmyk(hex)
 
-    sql_command = f"""INSERT INTO parent_colors_{color_space} (id, color_name, hex, rgb, lab, cmyk)
-    VALUES ({parent_id}, '{name}', '{hex}', CUBE(array[{r}, {g}, {b}]), CUBE(array[{lab_l}, {lab_a}, {lab_b}]), CUBE(array[{c}, {m}, {y}, {k}]));\n"""
+    closest_color_hex, closest_color_name = closest_color_in_space((hex, name), parent_colors_dict, color_space)
+    logging.info(f"Closest color computed: {closest_color_name} - {closest_color_hex}")
 
-    with open(f"insert_commands_{color_space}.sql", 'a') as f:
-        f.write(sql_command)
-
-def insert_color_name_sql_command(color_info, color_space, parent_ids, parent_colors, pantone):
-    hex, name = color_info
-    r, g, b = hex_to_rgb(hex)
-    lab_l, lab_a, lab_b = hex_to_lab(hex)
-    c, m, y, k = hex_to_cmyk(hex)
-
-    closest_color = closest_color_in_space((hex, name), parent_colors, color_space)
-    logging.info(f"Closest color computed: {closest_color}")
-
-    try:
-        closest_parent_color_id = parent_ids[closest_color]
-    except KeyError:
-        logging.warning(parent_ids)
-        logging.warning(f"Could not find closest color: {closest_color}. Skipping this entry.")
-        return
-
-    sql_command = f"""INSERT INTO color_names_{color_space} (color_name, hex, pantone, rgb, lab, cmyk, parent_color_id)
-    VALUES ('{name}', '{hex}', '{pantone}', CUBE(array[{r}, {g}, {b}]), CUBE(array[{lab_l}, {lab_a}, {lab_b}]), CUBE(array[{c}, {m}, {y}, {k}]), {closest_parent_color_id});\n"""
+    sql_command = f"""INSERT INTO color_names_{color_space} (color_name, hex, pantone, rgb, lab, cmyk, parent_color_name, parent_color_hex)
+    VALUES ('{name}', '{hex}', '{pantone}', CUBE(array[{r}, {g}, {b}]), CUBE(array[{lab_l}, {lab_a}, {lab_b}]), CUBE(array[{c}, {m}, {y}, {k}]), {closest_color_name}, {closest_color_hex});\n"""
 
     with open(f"insert_commands_{color_space}.sql", 'a') as f:
         f.write(sql_command)
